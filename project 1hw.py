6670debf254cb7b646e1879ef029ba391a3b7419
aaryanredkar <aaryantoki@live.com>
2015-11-25 08:33:04 -0800

    this program tells what you should or wear for a certain temperature

---
+++ project 1hw.py
@@ -0,0 +1,33 @@
+temp = int(input("What is the temperature out side?"))
+if temp >=90:
+
+    print ("Go to beach")
+
+elif temp >= 80:
+
+    print ("wear shorts")
+
+elif temp >=70:
+
+    print ("Wear full pants and a light jacket")
+
+elif temp >=60:
+
+    print ("Wear thick jacket")
+
+elif temp >=50:
+
+    print ("Wear Overcast")
+
+elif temp >=40:
+
+    print ("Stay Warm")
+
+elif temp ==30:
+
+    print ("Stay home")
+
+else:
+
+    print ("too cold to play outside must turn heater to full for full warmth")
+
